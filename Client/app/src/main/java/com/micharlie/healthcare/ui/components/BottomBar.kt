a01897b16419e454612fe0864dc93549c021d502
Carlo Enrique Guerra Vega <00052220@uca.edu.sv>
2024-06-13 13:14:29 -0600

    subiendo avances homeScreen

---
+++ Client/app/src/main/java/com/micharlie/healthcare/ui/components/BottomBar.kt
@@ -0,0 +1,2 @@
+package com.micharlie.healthcare.ui.components
+
--- Client/app/src/main/java/com/micharlie/healthcare/ui/screens/homeScreen/HomeScreen.kt
+++ Client/app/src/main/java/com/micharlie/healthcare/ui/screens/homeScreen/HomeScreen.kt
@@ -1,13 +1,44 @@
 package com.micharlie.healthcare.ui.screens.homeScreen
 
 import android.annotation.SuppressLint
+import android.media.Image
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.micharlie.healthcare.R
 import com.micharlie.healthcare.ui.components.DrawerBar
 import com.micharlie.healthcare.ui.components.TopBar
+import com.micharlie.healthcare.ui.theme.black
+import com.micharlie.healthcare.ui.theme.contrast1
+import com.micharlie.healthcare.ui.theme.contrast2
+import com.micharlie.healthcare.ui.theme.primary
+import com.micharlie.healthcare.ui.theme.secondary
+import com.micharlie.healthcare.ui.theme.white
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -15,12 +46,27 @@ fun HomeScreen() {
     val drawerState = rememberDrawerState(DrawerValue.Closed)
  DrawerBar(drawerState = drawerState, sessionState = false )
  {
-     Scaffold {
-            TopBar(drawerState = drawerState)
+     Scaffold (topBar = {TopBar(drawerState = drawerState) }){
+           Column(modifier = Modifier
+               .padding(it)
+               .fillMaxWidth()
+               .fillMaxHeight()
+               .background(color = primary))
+           {
+               HomeScreenContent()
+
+
+           }
 
      }
  }
 }
+@Composable
+fun HomeScreenContent(){
+
+
+}
+
 
 @Composable
 @Preview
