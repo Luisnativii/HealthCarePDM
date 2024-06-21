2413606a5b7dfe5c408d50279692715b2452a006
Carlo Enrique Guerra Vega <00052220@uca.edu.sv>
2024-06-20 18:23:54 -0600

    Fix VideoScreen, Funcionando

--- Client/app/src/main/java/com/micharlie/healthcare/ui/screens/VideoScreen/VideoScreen.kt
+++ Client/app/src/main/java/com/micharlie/healthcare/ui/screens/VideoScreen/VideoScreen.kt
@@ -2,6 +2,7 @@ package com.micharlie.healthcare.ui.screens.VideoScreen
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -9,12 +10,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -25,9 +32,12 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.micharlie.healthcare.data.api.VideoApi
 import com.micharlie.healthcare.ui.components.BottomBar
 import com.micharlie.healthcare.ui.components.DrawerBar
 import com.micharlie.healthcare.ui.components.TopBar
+import com.micharlie.healthcare.ui.components.VideoCard
+import com.micharlie.healthcare.ui.components.ViewModel.GetVideoState
 import com.micharlie.healthcare.ui.components.ViewModel.GetVideoViewModel
 import com.micharlie.healthcare.ui.components.YoutubePlayer
 import com.micharlie.healthcare.ui.theme.primary
@@ -44,18 +54,33 @@ fun VideoScreen(
     getVideoViewModel: GetVideoViewModel
 ) {
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    var videos by remember { mutableStateOf(listOf<VideoApi>()) }
+
+    LaunchedEffect(getVideoViewModel) {
+        getVideoViewModel.getVideoState.collect { state ->
+            when (state) {
+                is GetVideoState.Success -> {
+                    videos = state.videos
+                }
+                // Manejar otros estados si es necesario
+                else -> {}
+            }
+        }
+    }
     DrawerBar(drawerState = drawerState,
         sessionState = sessionState,
         navController = navController,
         getVideoViewModel = getVideoViewModel,
         content = {
             Scaffold(topBar = { TopBar(drawerState = drawerState) }, bottomBar = { BottomBar() }) {
+
                 Column(
                     modifier = Modifier
                         .padding(it)
                         .background(primary)
                         .fillMaxHeight()
-                        .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
                     Text(
                         text = name,
@@ -76,12 +101,27 @@ fun VideoScreen(
                     ) {
                         YoutubePlayer(videoId = url)
                     }
-//                    Text(
-//                        text = "Videos Similares",
-//                        fontSize = 20.sp,
-//                        color = white,
-//                        fontWeight = FontWeight.SemiBold
-//                    )
+//                    Column(modifier = Modifier.padding()) {
+//                        Text(text = "Videos Relacionados", fontSize = 20.sp, color = white, fontWeight = FontWeight.Bold)
+//                        LazyColumn(
+//                            modifier = Modifier.fillMaxWidth(),
+//                            horizontalAlignment = Alignment.CenterHorizontally
+//                        ) {
+//                            items(videos.size) { it1 ->
+//                                //VideoCard()
+//                                VideoCard(
+//                                    videoId = videos[it1].link!!,
+//                                    videoCategory = videos[it1].category!!,
+//                                    videoImageUrl = videos[it1].videoBanner!!,
+//                                    imageChannel = videos[it1].channelPhoto!!,
+//                                    videoTitle = videos[it1].videoName!!,
+//                                    userChannel = videos[it1].channelName!!,
+//                                    navController,
+//                                )
+//
+//                            }
+//                        }
+//                    }
 
                 }
 
