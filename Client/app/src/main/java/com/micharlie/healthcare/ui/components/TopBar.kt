194165a9ab6d84e3763710ac6d0009820f79f24d
iamcarlogg <00052220@uca.edu.sv>
2024-06-24 18:16:19 -0600

    Cambiando DrawerIcon, DrawerBar no session, quitando elementos de login en drawerBar

--- Client/app/src/main/java/com/micharlie/healthcare/ui/components/TopBar.kt
+++ Client/app/src/main/java/com/micharlie/healthcare/ui/components/TopBar.kt
@@ -3,16 +3,20 @@ package com.micharlie.healthcare.ui.components
 import androidx.compose.foundation.Image
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.micharlie.healthcare.R
 import com.micharlie.healthcare.ui.theme.primary
 import com.micharlie.healthcare.ui.theme.white
@@ -29,34 +33,24 @@ fun TopBar(drawerState: DrawerState) {
                 painter = painterResource(R.drawable.logo_blue), contentDescription
                 = "logo", contentScale = ContentScale.FillBounds
             )
+        }, navigationIcon = {
+            IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                Icon(imageVector = Icons.Rounded.Menu, contentDescription = "", tint = white)
+
+            }
         },
         //cambiando los colores de la barra
         colors
         = TopAppBarDefaults
             .topAppBarColors
                 (containerColor = primary), actions = {
-            IconButton(onClick = { /*TODO*/ },
-                //agregando el contenido del topbar
-                content = {
-                    //boton para desplegar el menu
-                    IconButton(
-                        onClick = { scope.launch { drawerState.open() } },
-                        content = {
-                            Icon(
-                                imageVector = Icons.Filled.Menu,
-                                contentDescription = "Menu",
-                                tint = white
-                            )
-                        }
-                    )
-                })
+
         })
 }
 
 
-//@Composable
-//@Preview
-//fun TopBarPreview()
-//{
-//    TopBar()
-//}
+@Composable
+@Preview
+fun TopBarPreview() {
+    TopBar(drawerState = rememberDrawerState(initialValue = DrawerValue.Closed))
+}
