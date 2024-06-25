9acea8f476957b0076a08ff9d8e3400c6b6cc646
00004722 <00004722@uca.edu.sv>
2024-06-24 21:04:02 -0600

    ADD: Hotfix🔥

--- Client/app/src/main/java/com/micharlie/healthcare/ui/signup/RegisterScreen.kt
+++ Client/app/src/main/java/com/micharlie/healthcare/ui/signup/RegisterScreen.kt
@@ -299,23 +299,21 @@ fun RegisterScreen(
                         val service = retrofit.create(UserApiService::class.java)
                         val call = service.postUser(user)
 
-                            call.enqueue(object : Callback<String> {
-                                override fun onResponse(call: Call<String>, response: Response<String>) {
-                                    if (response.isSuccessful) {
-                                        val token = response.body()
-                                        val t = token ?: "$token"
+                        call.enqueue(object : Callback<String> {
+                            override fun onResponse(call: Call<String>, response: Response<String>) {
+                                if (response.isSuccessful) {
+                                    val token = response.body()
+                                    val t = token ?: "$token"
 
-                                        val sharedPreferencesManager = SharedPreferencesManager(context)
+                                    val sharedPreferencesManager = SharedPreferencesManager(context)
 
+                                    sharedPreferencesManager.saveToken(t)
+                                    sharedPreferencesManager.saveEmail(email)
+                                    sharedPreferencesManager.saveEmail(email)  // Guarda el correo electrónico
+                                    navController.navigate(ScreenRoute.HomeSession.route)
 
-                                        sharedPreferencesManager.saveToken(t)
-                                        sharedPreferencesManager.saveEmail(email)
-                                        sharedPreferencesManager.saveEmail(email)  // Guarda el correo electrónico
-                                        navController.navigate(ScreenRoute.HomeSession.route)
-
-                                    } else {
-                                        errorMessage = "Error desconocido."
-                                    }
+                                } else {
+                                    errorMessage = "Error desconocido."
                                 }
                             }
 
